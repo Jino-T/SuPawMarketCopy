0af1ce5ee0aea57e6961a1c06a96c11240e7c2f0
Seth Owirodu <sowirodu@trinity.edu>
2024-03-03 12:38:32 -0600

    Navbaring

--- SuPawMarket.js
+++ SuPawMarket.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 // Set the view engine to ejs if you are rendering views
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'views')))
-app.use(express.static(path.join(__dirname, 'public')))
+// Added views and public folder
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Require the module you just created
-const myApi = require('./routes/myApi');
+const myApi = require("./routes/myApi");
 
 // Use the module with your Express application at the root path
-app.use('/', myApi); // Mount at the root path
+app.use("/", myApi); // Mount at the root path
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
