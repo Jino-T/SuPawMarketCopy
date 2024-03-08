976b3f3c54832d2404fbd7b904fcbc853a3007e3
Justin Burns II <jburns1@trinity.edu>
2024-03-07 18:52:39 -0600

    Functioning login and account creation from frontend

--- SuPawMarket.js
+++ SuPawMarket.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 var connection = require('./database').databaseConnection;
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Set the view engine to ejs if you are rendering views
 app.set("view engine", "ejs");
@@ -16,6 +19,11 @@ const myApi = require("./routes/myApi");
 
 // Use the module with your Express application at the root path
 app.use("/", myApi); // Mount at the root path
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routes/myApi.js
+++ routes/myApi.js
@@ -1,7 +1,16 @@
 // myApi.js - My API route module
 
 const express = require("express");
+const UserController = require("../controllers/UserController");
 const router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 router.get("/", function (req, res) {
     res.render("pages/home"); // This will render views/pages/home.ejs
@@ -22,7 +31,31 @@ router.get("/login", function (req, res) {
   res.render("pages/login"); // This will render views/pages/login.ejs
 });
 
+router.post("/validate",urlencodedParser, async function(req, res) {
+  //console.log(req);
+    let check = await UserController.validate(req.body);
+    console.log(check);
+    if(check === true) {
+      res.render("pages/home");
+    }
+    else {
+      res.send("Wrong username or password");
+    }
   
+})
+
+router.post("/create",urlencodedParser, async function(req, res) {
+  //console.log(req);
+    let check = await UserController.createUser(req.body);
+    console.log(check);
+    if(check === true) {
+      res.render("pages/home");
+    }
+    else {
+      res.send("Username already in use");
+    }
   
+})
+
 
 module.exports = router;
