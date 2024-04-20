56556ab1eb303403241281eb223a48ff4b7394d3
Jino Tesauro <jinotesauro@pop-os.localdomain>
2024-04-20 18:06:41 -0500

    Fixed incorrect database statements

--- SQL/deleteTables.sql
+++ SQL/deleteTables.sql
@@ -1,9 +1,10 @@
 DROP TABLE `supawdb`.`userAddress`;
-DROP TABLE `supawdb`.`inCategory`;
+DROP TABLE `supawdb`.`incategory`;
 DROP TABLE `supawdb`.`address`;
 DROP TABLE `supawdb`.`category`;
 DROP TABLE `supawdb`.`purchase`;
 DROP TABLE `supawdb`.`review`;
 DROP TABLE `supawdb`.`cart`;
+DROP TABLE `supawdb`.`edits`;
 DROP TABLE `supawdb`.`user`;
 DROP TABLE `supawdb`.`product`;
